af4cefe52f16c384bd0d2f1e35073b52e5307321
sseen <ssnfee@gmail.com>
2017-07-18 10:11:57 +0800

    crash code

--- RxswiftCollectionDataSourceIssueDemo/RDHomeCollectionFlowLayout.swift
+++ RxswiftCollectionDataSourceIssueDemo/RDHomeCollectionFlowLayout.swift
@@ -31,22 +31,39 @@ class RDHomeCollectionFlowLayout: UICollectionViewFlowLayout {
         var layoutAttributes = [UICollectionViewLayoutAttributes]()
 
         // Cells
-        let attributesArray = super.layoutAttributesForElements(in: rect);
-        guard self.collectionView != nil else {return attributesArray;}
-        for attributes in attributesArray ?? [] {
-            
-            attributes.frame = CGRect(x: (self.itemWidth() + 1 ) * (CGFloat)(attributes.indexPath.item) ,
-                                      y: RD.CustomCollection.HourHeaderWidth * (CGFloat)(attributes.indexPath.item % 4),
-                                      width: self.itemWidth(),
-                                      height: RD.CustomCollection.HourHeaderWidth)
-            
-            layoutAttributes.append(attributes)
+        let visiableIndexPaths = self.indexPathsOfItemsInRect(rect)
+        for indexPath:IndexPath in visiableIndexPaths {
+            let  attributes = self.layoutAttributesForItem(at: indexPath)
+            layoutAttributes.append(attributes!)
         }
         
         
         print("\(layoutAttributes.count)")
         
         return layoutAttributes
+    }
+    
+    func indexPathsOfItemsInRect(_ rect:CGRect) -> [IndexPath] {
+        var indexPaths = [IndexPath]()
+        if datas.count > 0 {
+            for index in datas[0].items {
+                let a_index = IndexPath(item: index, section: 0)
+                indexPaths.append(a_index)
+            }
+        }
+        
+        return indexPaths
+    }
+    
+    
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+        attributes.frame = CGRect(x: (self.itemWidth() + 1 ) * (CGFloat)(indexPath.item) ,
+                                  y: RD.CustomCollection.HourHeaderWidth * (CGFloat)(indexPath.item % 4),
+                                  width: self.itemWidth(),
+                                  height: RD.CustomCollection.HourHeaderWidth)
+        
+        return attributes
     }
     
     func itemWidth() -> CGFloat {
